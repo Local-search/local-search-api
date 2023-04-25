fc7c1d14cc57d9567d2f573e088ac6c9f9605ca4
NGR-NP <ngr112001@gmail.com>
2023-04-25 20:58:37 +0545

    counting ads by their status

--- src/count/docCount.routes.js
+++ src/count/docCount.routes.js
@@ -1,8 +1,19 @@
-const { countCatg, countKeyword, countBusiness } = require("./docCount.controller");
+const { verifyJwt, verifyAdmin } = require("../auth/auth.middleware");
+const {
+  countCatg,
+  countKeyword,
+  countBusiness,
+  countAds,
+  countUser,
+} = require("./docCount.controller");
 
 const docCountRoutes = require("express").Router();
+docCountRoutes.use(verifyJwt);
+docCountRoutes.use(verifyAdmin);
 docCountRoutes.get("/catg", countCatg);
 docCountRoutes.get("/keyword", countKeyword);
 docCountRoutes.get("/business", countBusiness);
+docCountRoutes.get("/ads", countAds);
+docCountRoutes.get("/user", countUser);
 
 module.exports = docCountRoutes;
