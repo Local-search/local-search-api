8ce6ea65e4c21a7446b9fa916c620da9f351cdc5
NGR <72463407+NGR-NP@users.noreply.github.com>
2023-04-29 14:49:43 +0545

    commit

--- src/businessProfile/businessProfile.controller.js
+++ src/businessProfile/businessProfile.controller.js
@@ -247,7 +247,7 @@ exports.deleteBusinessProfileById = async (req, res, next) => {
 exports.trendingBusiness = async (req, res, next) => {
   try {
     const businesses = await BusinessProfile.find({ status: "true" })
-      .sort({ popular: -1 })
+      // .sort({ popular: -1 })
       .limit(4);
     res.status(200).json(businesses);
   } catch (err) {
