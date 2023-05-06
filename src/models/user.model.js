fab5afb37da56b90cf6abbe466e37129825d59da
NGR <72463407+NGR-NP@users.noreply.github.com>
2023-05-06 17:02:28 +0545

    commit

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -30,7 +30,7 @@ const UserSchema = new mongoose.Schema(
     phone: {
       type: Number,
       required: "phone number is required",
-      max: 13,
+      max: 10,
     },
     role: {
       type: String,
