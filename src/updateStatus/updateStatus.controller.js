d52b1bc6326193c185345b8dfd41e352b725fbba
NGR <72463407+NGR-NP@users.noreply.github.com>
2023-05-17 23:19:12 +0545

    commit

--- src/updateStatus/updateStatus.controller.js
+++ src/updateStatus/updateStatus.controller.js
@@ -23,29 +23,29 @@ const UpdateStatus = async (req, res, next, model, update, query,) => {
 }
 const businessStatus = async (req, res, next) => {
     const { status } = req.query
-    const query = { status: status }
-    if (!status || status === undefined) {
-        next(ERROR(400, "status can only be true or false!!!"))
-    } else {
-        UpdateStatus(req, res, next, businessProfileModel, update = "status", query)
-    }
-    // console.log("status", status)
-    // const { id } = req.params
-
-    // try {
-    //     const updateStatus = await businessProfileModel.findByIdAndUpdate(id,
-    //         {
-    //             $set: { status }
-    //         },
-    //         { new: true }
-    //     ).select("status")
-    //     console.log(updateStatus)
-    //     if (!updateStatus) {
-    //         return next(ERROR(404, "there is no data with accosicate id to update!"));
-    //     }
-    //     res.status(201).json(updateStatus);
-    // } catch (err) {
-    //     next(err)
+    // const query = { status: status }
+    // if (!status || status === undefined) {
+    //     next(ERROR(400, "status can only be true or false!!!"))
+    // } else {
+    //     UpdateStatus(req, res, next, businessProfileModel, update = "status", query)
     // }
+    // console.log("status", status)
+    const { id } = req.params
+
+    try {
+        const updateStatus = await businessProfileModel.findByIdAndUpdate(id,
+            {
+                $set: { status }
+            },
+            { new: true }
+        ).select("status")
+        console.log(updateStatus)
+        if (!updateStatus) {
+            return next(ERROR(404, "there is no data with accosicate id to update!"));
+        }
+        res.status(201).json(updateStatus);
+    } catch (err) {
+        next(err)
+    }
 }
 module.exports = { businessStatus }
