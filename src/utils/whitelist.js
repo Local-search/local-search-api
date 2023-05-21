558c2d5a0f5e43cf26bdb2b5f2ca332c3f5ed158
NGR <72463407+NGR-NP@users.noreply.github.com>
2023-05-21 10:38:02 +0545

    commit

--- src/utils/whitelist.js
+++ src/utils/whitelist.js
@@ -1,5 +1,6 @@
 const whitelist = [
   "https://local-search-itahari-dev.netlify.app",
+  "https://ngr-np-ideal-winner-j9vvvp5qj6j2jqq9-5173.preview.app.github.dev",
   "http://localhost:5173",
 ];
 module.exports = whitelist;
